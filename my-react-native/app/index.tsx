eb0fe458c8e44f863c6774320bcdc69ebe7ef1a5
rutkat <{rutkat}@users.noreply.github.com>
2025-08-15 22:38:46 -0400

    Added text and bullet list.

--- my-react-native/app/index.tsx
+++ my-react-native/app/index.tsx
@@ -4,7 +4,19 @@ export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.bodyContainer}>
-        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text style={styles.bodyContainer}>
+          <Text style={styles.bodyTitle}>
+            How It Works
+          </Text>
+          <Text>
+            {'\n\n'}Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+            {'\n\n'}Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+            {'\n'}
+          </Text>
+          <Text>{'\n\u2022'} Bullet-style List Item 1</Text>
+          <Text>{'\n\u2022'} Bullet-style List Item 2</Text>
+          <Text>{'\n\u2022'} Bullet-style List Item 3</Text>
+        </Text>
       </View>
     </SafeAreaView>
   );
