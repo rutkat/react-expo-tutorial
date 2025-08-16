9577f551b096ca434b23b349b20bfb94ad15834a
rutkat <{rutkat}@users.noreply.github.com>
2025-08-15 22:01:55 -0400

    Step01 complete

--- my-react-native/app/index.tsx
+++ my-react-native/app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.bodyContainer}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    color: '#00cbff;',
+    backgroundColor: '#FFF',
+    marginBottom: 20,
+  },
+  bodyContainer: {
+    marginHorizontal: 18,
+    padding: 18,
+    fontSize: 14,
+    textAlign: 'center',
+  },
+});
