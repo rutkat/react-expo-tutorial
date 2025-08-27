559b3d7513db1563d80d027f7379803541a9dd19
rutkat <{rutkat}@users.noreply.github.com>
2025-08-27 19:53:05 -0400

    Uncomment style.

--- my-react-native/components/menu.tsx
+++ my-react-native/components/menu.tsx
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   button: {
-    // fontFamily: "SpaceMono", 
+    fontFamily: "SpaceMono", 
     fontSize: 12,
     textDecorationLine: 'none',
     color: '#00cbff',
